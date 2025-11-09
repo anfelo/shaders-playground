dfedc1546f22fef399deba8a9cbe905d894c4dd0
Andres Osorio <af.osorio1341@gmail.com>
2025-11-09 22:07:56 +0100

    add parameters to simple shapes view

--- src/views/SimpleShapesView.vue
+++ src/views/SimpleShapesView.vue
@@ -24,7 +24,11 @@ const fragmentShader = `
 varying vec2 v_uvs;
 
 uniform vec2 u_resolution;
+uniform vec2 u_pos;
 uniform vec3 u_color;
+uniform int u_shape;
+uniform float u_rot;
+uniform float u_scale;
 
 #define PI 3.14159265359
 #define TWO_PI 6.28318530718
@@ -94,6 +98,13 @@ float sdf_hexagon(in vec2 p,in float r ) {
     return length(p)*sign(p.y);
 }
 
+mat2 rotate_2D(float angle) {
+    float s = sin(angle);
+    float c = cos(angle);
+
+    return mat2(c, -s, s, c);
+}
+
 void main(){
     vec2 pixel_coords = (v_uvs - 0.5) * u_resolution;
 
@@ -101,21 +112,39 @@ void main(){
     color = draw_grid(color, vec3(0.5), 10.0, 1.0);
     color = draw_grid(color, vec3(0.0), 100.0, 2.0);
 
-    // Circle
-    //float d = sdf_circle(pixel_coords, 100.0);
-    //color = mix(RED, color, step(0.0, d));
+    float d = 0.0;
 
+    vec2 pos = pixel_coords;
+    // Transformations: Got to apply the inverse transformation to get the shape
+    // to the place that we want it to be.
+    pos -= u_pos;
+    pos *= rotate_2D(u_rot);
+
+    switch (u_shape) {
     // Line
-    //float d = sdf_line(pixel_coords, vec2(-100.0, -50.0), vec2(200.0, -75.0));
-    //color = mix(RED, color, step(5.0, d));
-
+    case 0:
+        d = sdf_line(pos, vec2(-100.0, -50.0) * u_scale, vec2(200.0, -75.0) * u_scale);
+        color = mix(u_color, color, step(5.0, d));
+        break;
+    // Circle
+    case 1:
+        d = sdf_circle(pos, 100.0 * u_scale);
+        color = mix(u_color * 0.5, color, smoothstep(-1.0, 1.0, d));
+        color = mix(u_color, color, smoothstep(-5.0, 0.0, d));
+        break;
     // Box
-    // float d = sdf_box(pixel_coords, vec2(300.0, 100.0));
-    // color = mix(RED, color, step(0.0, d));
-
+    case 2:
+        d = sdf_box(pos, vec2(300.0, 100.0) * u_scale);
+        color = mix(u_color * 0.5, color, smoothstep(-1.0, 1.0, d));
+        color = mix(u_color, color, smoothstep(-5.0, 0.0, d));
+        break;
     // Hexagon
-    float d = sdf_hexagon(pixel_coords, 300.0);
-    color = mix(RED, color, step(0.0, d));
+    case 3:
+        d = sdf_hexagon(pos, 300.0 * u_scale);
+        color = mix(u_color * 0.5, color, smoothstep(-1.0, 1.0, d));
+        color = mix(u_color, color, smoothstep(-5.0, 0.0, d));
+        break;
+    }
 
     gl_FragColor = vec4(color, 1.0);
 }
@@ -134,7 +163,12 @@ class SimpleShapesScene implements Scene {
 
   gui = new GUI()
   uiState = {
-    u_color: [0.0, 0.0, 0.0],
+    u_color: [1.0, 0.0, 0.0],
+    u_shape: 1,
+    u_pos_x: 0.0,
+    u_pos_y: 0.0,
+    u_rot: 0.0,
+    u_scale: 1.0
   }
 
   constructor() {}
@@ -177,6 +211,10 @@ class SimpleShapesScene implements Scene {
     this.uniforms = {
       u_resolution: { value: [window.innerWidth, window.innerHeight] },
       u_color: { value: this.uiState.u_color },
+      u_shape: { value: this.uiState.u_shape },
+      u_pos: { value: this.uiState.u_pos },
+      u_rot: { value: this.uiState.u_rot },
+      u_scale: { value: this.uiState.u_scale },
     }
     const material = new THREE.ShaderMaterial({
       uniforms: this.uniforms,
@@ -194,6 +232,10 @@ class SimpleShapesScene implements Scene {
   private animate(_time: DOMHighResTimeStamp, _frame: XRFrame): void {
     this.uniforms.u_resolution.value = [window.innerWidth, window.innerHeight]
     this.uniforms.u_color.value = this.uiState.u_color
+    this.uniforms.u_shape.value = this.uiState.u_shape
+    this.uniforms.u_pos.value = [this.uiState.u_pos_x, this.uiState.u_pos_y]
+    this.uniforms.u_rot.value = (this.uiState.u_rot * Math.PI) / 180
+    this.uniforms.u_scale.value = this.uiState.u_scale
 
     this.renderer.render(this.scene, this.camera)
   }
@@ -205,6 +247,17 @@ class SimpleShapesScene implements Scene {
 
   private initDebugUI() {
     this.gui.addColor(this.uiState, 'u_color')
+    this.gui.add(this.uiState, 'u_shape', { Line: 0, Circle: 1, Box: 2, Hexagon: 3 })
+
+    const posFolder = this.gui.addFolder( 'Position' );
+    posFolder.add(this.uiState, 'u_pos_x', -300.0, 300, 0.01);
+    posFolder.add(this.uiState, 'u_pos_y', -300.0, 300, 0.01);
+
+    const rotFolder = this.gui.addFolder( 'Rotation' );
+    rotFolder.add(this.uiState, 'u_rot', 0, 360, 0.01);
+
+    const scaleFolder = this.gui.addFolder( 'Scale' );
+    scaleFolder.add(this.uiState, 'u_scale', 1, 10, 0.01);
   }
 }
 
